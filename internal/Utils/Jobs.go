94132d69a21dc9b523352e5d300fce2f733239d4
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2026-06-02 19:17:52 +0530

    refactor: commented saving image on disk

--- internal/Utils/Jobs.go
+++ internal/Utils/Jobs.go
@@ -21,7 +21,7 @@ func HandleImageJob(name, path, token, repo string, generator func() (image.Imag
 		log.Printf("Gen Error (%s): %v\n", name, err)
 		return
 	}
-	SaveImage(name+".png", img)
+	// SaveImage(name+".png", img)
 
 	// 2. Format Config (Using your utils package)
 	cfg, err := FormmatUpload(token, repo, path, "main", "Update "+name)
